ba80a548285f9ebaac663909ad16945c6cd65a56
HelloWorld <name201>
2020-08-26 17:16:30 +0800

    提交成功

---
+++ Javaweb01/src/com/hh/Gittest.java
@@ -0,0 +1,11 @@
+package com.hh;
+
+/**
+ * @author xcc
+ * @date 2020/8/26 - 16:35
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("更新1");
+    }
+}
